b319c3cccaa27f9c7d927865b1f74a9b2fbd69c9
emillyalmeida <emillyalmeidan@gmail.com>
2022-05-14 18:02:16 -0300

    feat: salve for commit merge with develop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Signup from "./page/Signup";
 import GlobalStyle from "./styles/global";
 import Routers from "./routers";
-import Header from "./Components/Header";
+import Header from "./components/Header";
 
 function App() {
   return (
