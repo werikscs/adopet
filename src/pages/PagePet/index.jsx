f45b14be107401b930e06db46cad29a6850fc8a4
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-05-14 13:18:25 -0300

    feat: implemement pagepet page

--- src/pages/PagePet/index.jsx
+++ src/pages/PagePet/index.jsx
@@ -1,3 +1,83 @@
-const PagePet = () => {};
+import defaultImg from "../../assets/proprietario-default-img.svg";
+import ButtonOutlined from "../../components/ButtonOutlined";
+import Button from "../../components/Button";
+
+import {
+  GenericContainer,
+  Main,
+  Figcaption,
+  DivMoreImgs,
+  DivInfoPet,
+  DivInfoUser,
+  DivButtons,
+} from "./styles";
+
+const PagePet = () => {
+  return (
+    <GenericContainer>
+      <Main>
+        {/* imagem grande do animal */}
+        <Figcaption>
+          <img
+            src="https://uploaddeimagens.com.br/images/003/866/666/original/cao1_1.png"
+            alt=""
+          />
+        </Figcaption>
+
+        {/* outras imagens do animal */}
+        <DivMoreImgs>
+          <img
+            src="https://uploaddeimagens.com.br/images/003/866/666/original/cao1_1.png"
+            alt=""
+          />
+          <img
+            src="https://uploaddeimagens.com.br/images/003/866/666/original/cao1_1.png"
+            alt=""
+          />
+          <img
+            src="https://uploaddeimagens.com.br/images/003/866/666/original/cao1_1.png"
+            alt=""
+          />
+          <img
+            src="https://uploaddeimagens.com.br/images/003/866/666/original/cao1_1.png"
+            alt=""
+          />
+        </DivMoreImgs>
+
+        <DivInfoPet>
+          <h1>Fiona</h1>
+
+          <DivInfoUser>
+            <img src={defaultImg} alt="" />
+            <div>
+              <h2>João da Silva</h2>
+              <h3>proprietário</h3>
+            </div>
+          </DivInfoUser>
+
+          <h2>Fêmea</h2>
+
+          <h2>Porte Pequeno</h2>
+
+          <span>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+            hendrerit massa ipsum, nec aliquet ante varius non. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Quisque hendrerit massa
+            ipsum, nec aliquet ante varius non.Lorem ipsum dolor sit am
+          </span>
+        </DivInfoPet>
+
+        <DivButtons>
+          <ButtonOutlined callback={() => console.log("voltar")}>
+            voltar
+          </ButtonOutlined>
+          <Button onClick={() => console.log("Quero Adotar")} orangeSchema>
+            Quero Adotar
+          </Button>
+        </DivButtons>
+      </Main>
+    </GenericContainer>
+  );
+};
 
 export default PagePet;
---
+++ src/pages/PagePet/styles.js
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+
+export const GenericContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+
+  background-color: var(--color-second-50);
+
+  display: flex;
+  justify-content: center;
+`;
+
+export const Main = styled.main`
+  width: 100%;
+  max-width: 768px;
+  min-width: var(--min-width);
+
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+
+  padding: 16px;
+
+  background-color: var(--color-second-50);
+`;
+
+export const Figcaption = styled.figcaption`
+  width: 100%;
+
+  img {
+    display: block;
+
+    width: 100%;
+
+    object-fit: cover;
+
+    border-radius: 8px;
+  }
+`;
+
+export const DivMoreImgs = styled.div`
+  width: 100%;
+  height: fit-content;
+
+  display: flex;
+  gap: 16px;
+
+  overflow-x: auto;
+
+  img {
+    width: 72px;
+    height: 72px;
+
+    object-fit: cover;
+
+    border-radius: 8px;
+  }
+`;
+
+export const DivInfoUser = styled.div`
+  width: 100%;
+
+  display: flex;
+  gap: 12px;
+
+  img {
+    width: 50px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    h2 {
+      font-size: 1.25rem;
+
+      color: var(--color-title);
+    }
+
+    h3 {
+      font-size: 1rem;
+
+      color: var(--color-title-50);
+
+      margin-top: -10px;
+    }
+  }
+`;
+
+export const DivInfoPet = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+
+  span {
+    font-size: 1rem;
+  }
+`;
+
+export const DivButtons = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  background-color: var(--color-second-50);
+`;
