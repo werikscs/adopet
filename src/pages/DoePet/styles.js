96faed9cb6ee6fbad0eb86bf96eb682628ce631d
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-05-14 19:37:07 -0300

    refactor: remove textarea style from styles.js

--- src/pages/DoePet/styles.js
+++ src/pages/DoePet/styles.js
@@ -47,30 +47,6 @@ export const Form = styled.form`
   gap: 16px;
 `;
 
-export const DivTextArea = styled.div`
-  width: 100%;
-
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-
-  label {
-    font-size: 1.25rem;
-    font-weight: 600;
-
-    padding-left: 16px;
-  }
-
-  textarea {
-    resize: none;
-
-    padding: 16px;
-
-    border: 2px solid var(--color-first);
-    border-radius: 8px;
-  }
-`;
-
 export const DivButtons = styled.div`
   display: flex;
   justify-content: space-between;
