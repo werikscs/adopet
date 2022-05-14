a325b77795a852c3244d97d0e03f5652b197b913
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-05-14 10:47:31 -0300

    refactor: change routes

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -11,26 +11,39 @@ import PagePet from "../pages/PagePet";
 const Routers = () => {
   return (
     <Switch>
+      {/* */}
       <Route exact path="/">
         <Home />
       </Route>
-      <Route path="/Cadastro">
+
+      {/*  */}
+      <Route path="/cadastro">
         <Cadastro />
       </Route>
-      <Route path="/Login">
+
+      {/*  */}
+      <Route path="/login">
         <Login />
       </Route>
-      <Route path="/Adote">
+
+      {/* página com a lista de animais */}
+      <Route path="/adote">
         <Adote />
       </Route>
-      <Route path="/Doe-um-pet">
-        <DoePet />
-      </Route>
-      <Route path="/Pet/:name">
+
+      {/* página do detalhe do pet */}
+      <Route path="/adote/:id">
         <PagePet />
       </Route>
-      <Route path="/Dashboard">
+
+      {/* página dos pets que o usuário cadastrou na plataforma (somente logado) */}
+      <Route path="/user/pets">
         <Dashboard />
+      </Route>
+
+      {/* página com tela de cadastro de animal (somente logado) */}
+      <Route path="/user/doe">
+        <DoePet />
       </Route>
     </Switch>
   );
