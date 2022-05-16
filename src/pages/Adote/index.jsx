97113ba71c8f064e2125d7879fc00085268598b9
emillyalmeida <emillyalmeidan@gmail.com>
2022-05-16 12:48:39 -0300

    feat: add logic in index.jsx

--- src/pages/Adote/index.jsx
+++ src/pages/Adote/index.jsx
@@ -1,3 +1,66 @@
-const Adote = () => {};
+import { useState } from "react";
+import DivSelect from "../../components/DivSelect";
+import api from "../../services/api";
+import { ContainerTotal, ContentFiltro, ContentList } from "./styles";
+
+const Adote = () => {
+  const [sex, setSex] = useState("");
+  const [species, setSpecies] = useState("");
+  const [size, setSizes] = useState("");
+
+  const filtros = () => {
+    api.get(`/animals?${sex}${species}${size}`).then((res) => {
+      console.log(res.data);
+    });
+  };
+
+  filtros();
+  return (
+    <ContainerTotal>
+      <ContentFiltro>
+        <div>
+          <DivSelect
+            label="Porte"
+            placeholder="qualquer"
+            name="size="
+            // register={register}
+            error={""}
+            setSelect={setSizes}
+          >
+            <option value="small">Pequeno</option>
+            <option value="medium">Medio</option>
+            <option value="large">Grande</option>
+          </DivSelect>
+
+          <DivSelect
+            label="Tipo"
+            placeholder="qualquer"
+            name="species="
+            // register={register}
+            error={""}
+            setSelect={setSpecies}
+          >
+            <option value="cat">Gato</option>
+            <option value="dog">Cachorro</option>
+          </DivSelect>
+
+          <DivSelect
+            label="Sexo"
+            type="text"
+            placeholder="qualquer"
+            name="sex="
+            // register={register}
+            error={""}
+            setSelect={setSex}
+          >
+            <option value="f">Femea</option>
+            <option value="m">Macho</option>
+          </DivSelect>
+        </div>
+      </ContentFiltro>
+      <ContentList></ContentList>
+    </ContainerTotal>
+  );
+};
 
 export default Adote;
