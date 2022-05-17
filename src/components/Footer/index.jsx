69c9cd8d311f51a1b937467a75641cabc257abaa
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-05-16 21:37:29 -0300

    refactor: rename paragraph__title--footer to ADOPET

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer footer__home">
       <div className="content has-text-centered">
-        <p className="paragraph__title--footer">Apadote</p>
+        <p className="paragraph__title--footer">ADOPET</p>
         <p className="paragraph__footer">Todos os direitos reservados</p>
       </div>
     </footer>
