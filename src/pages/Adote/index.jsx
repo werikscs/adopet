a8d701749db10804eaef2017fcec1ebf9c9bce43
emillyalmeida <emillyalmeidan@gmail.com>
2022-05-17 12:16:26 -0300

    feat: add container in adote

--- src/pages/Adote/index.jsx
+++ src/pages/Adote/index.jsx
@@ -6,7 +6,7 @@ import ListCard from "../../components/ListCard";
 
 import api from "../../services/api";
 
-import { ContainerTotal, ContentFiltro, ContentList } from "./styles";
+import { Container, ContentTotal, ContentFiltro, ContentList } from "./styles";
 import iconFilter from "../../assets/filterIcon.svg";
 
 import {
@@ -45,131 +45,133 @@ const Adote = () => {
   // const btnRef = React.useRef()
 
   return (
-    <ContainerTotal>
-      <ContentFiltro>
-        <div>
-          <h2>
+    <Container>
+      <ContentTotal>
+        <ContentFiltro>
+          <div>
+            <h2>
+              <img src={iconFilter} alt="icon filter" /> Pesquisar por filtro
+            </h2>
+            <InputSeach setName={setName} />
+            <DivSelect
+              label="PORTE"
+              placeholder="qualquer"
+              name="size="
+              error={""}
+              setSelect={setSizes}
+            >
+              <option value="small">Pequeno</option>
+              <option value="medium">Medio</option>
+              <option value="large">Grande</option>
+            </DivSelect>
+
+            <DivSelect
+              label="ESPECIE"
+              placeholder="qualquer"
+              name="species="
+              error={""}
+              setSelect={setSpecies}
+            >
+              <option value="cat">Gato</option>
+              <option value="dog">Cachorro</option>
+            </DivSelect>
+
+            <DivSelect
+              label="SEXO"
+              type="text"
+              placeholder="qualquer"
+              name="sex="
+              error={""}
+              setSelect={setSex}
+            >
+              <option value="f">Femea</option>
+              <option value="m">Macho</option>
+            </DivSelect>
+          </div>
+          {/* mobile */}
+          <button onClick={onOpen}>
             <img src={iconFilter} alt="icon filter" /> Pesquisar por filtro
-          </h2>
-          <InputSeach setName={setName} />
-          <DivSelect
-            label="PORTE"
-            placeholder="qualquer"
-            name="size="
-            error={""}
-            setSelect={setSizes}
+          </button>
+          <Drawer
+            isOpen={isOpen}
+            placement="right"
+            onClose={onClose}
+            // finalFocusRef={btnRef}
           >
-            <option value="small">Pequeno</option>
-            <option value="medium">Medio</option>
-            <option value="large">Grande</option>
-          </DivSelect>
+            <DrawerOverlay />
+            <DrawerContent bgColor="var(--color-second)">
+              <DrawerHeader
+                display="flex"
+                flexDirection="row"
+                alignItems="center"
+                justifyContent="space-around"
+                color="var(--color-first)"
+                fontWeight="600"
+                fontSize="1.4rem"
+                borderBottomColor="var(--color-first)"
+                borderBottomWidth="1px"
+                pb="10px"
+                pt="10px"
+              >
+                <img src={iconFilter} alt="icon filter" /> Pesquisar por Filtro
+                <DrawerCloseButton />
+              </DrawerHeader>
 
-          <DivSelect
-            label="ESPECIE"
-            placeholder="qualquer"
-            name="species="
-            error={""}
-            setSelect={setSpecies}
-          >
-            <option value="cat">Gato</option>
-            <option value="dog">Cachorro</option>
-          </DivSelect>
+              <DrawerBody
+                display="flex"
+                flexDirection="column"
+                w="100%"
+                pl="15px"
+                // overflowY="scroll"
+              >
+                <div>
+                  <InputSeach setName={setName} />
+                  <DivSelect
+                    label="PORTE"
+                    placeholder="qualquer"
+                    name="size="
+                    error={""}
+                    setSelect={setSizes}
+                  >
+                    <option value="small">Pequeno</option>
+                    <option value="medium">Medio</option>
+                    <option value="large">Grande</option>
+                  </DivSelect>
 
-          <DivSelect
-            label="SEXO"
-            type="text"
-            placeholder="qualquer"
-            name="sex="
-            error={""}
-            setSelect={setSex}
-          >
-            <option value="f">Femea</option>
-            <option value="m">Macho</option>
-          </DivSelect>
-        </div>
-        {/* mobile */}
-        <button onClick={onOpen}>
-          <img src={iconFilter} alt="icon filter" /> Pesquisar por filtro
-        </button>
-        <Drawer
-          isOpen={isOpen}
-          placement="right"
-          onClose={onClose}
-          // finalFocusRef={btnRef}
-        >
-          <DrawerOverlay />
-          <DrawerContent bgColor="var(--color-second)">
-            <DrawerHeader
-              display="flex"
-              flexDirection="row"
-              alignItems="center"
-              justifyContent="space-around"
-              color="var(--color-first)"
-              fontWeight="600"
-              fontSize="1.4rem"
-              borderBottomColor="var(--color-first)"
-              borderBottomWidth="1px"
-              pb="10px"
-              pt="10px"
-            >
-              <img src={iconFilter} alt="icon filter" /> Pesquisar por Filtro
-              <DrawerCloseButton />
-            </DrawerHeader>
+                  <DivSelect
+                    label="ESPECIE"
+                    placeholder="qualquer"
+                    name="species="
+                    error={""}
+                    setSelect={setSpecies}
+                  >
+                    <option value="cat">Gato</option>
+                    <option value="dog">Cachorro</option>
+                  </DivSelect>
 
-            <DrawerBody
-              display="flex"
-              flexDirection="column"
-              w="100%"
-              pl="15px"
-              // overflowY="scroll"
-            >
-              <div>
-                <InputSeach setName={setName} />
-                <DivSelect
-                  label="PORTE"
-                  placeholder="qualquer"
-                  name="size="
-                  error={""}
-                  setSelect={setSizes}
-                >
-                  <option value="small">Pequeno</option>
-                  <option value="medium">Medio</option>
-                  <option value="large">Grande</option>
-                </DivSelect>
+                  <DivSelect
+                    label="SEXO"
+                    type="text"
+                    placeholder="qualquer"
+                    name="sex="
+                    error={""}
+                    setSelect={setSex}
+                  >
+                    <option value="f">Femea</option>
+                    <option value="m">Macho</option>
+                  </DivSelect>
+                </div>
+              </DrawerBody>
 
-                <DivSelect
-                  label="ESPECIE"
-                  placeholder="qualquer"
-                  name="species="
-                  error={""}
-                  setSelect={setSpecies}
-                >
-                  <option value="cat">Gato</option>
-                  <option value="dog">Cachorro</option>
-                </DivSelect>
-
-                <DivSelect
-                  label="SEXO"
-                  type="text"
-                  placeholder="qualquer"
-                  name="sex="
-                  error={""}
-                  setSelect={setSex}
-                >
-                  <option value="f">Femea</option>
-                  <option value="m">Macho</option>
-                </DivSelect>
-              </div>
-            </DrawerBody>
-
-            <DrawerFooter></DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-      </ContentFiltro>
-      <ContentList>
-        <ListCard listPets={listpets} isAdote />
-      </ContentList>
-    </ContainerTotal>
+              <DrawerFooter></DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+        </ContentFiltro>
+        <ContentList>
+          <ListCard listPets={listpets} isAdote />
+        </ContentList>
+      </ContentTotal>
+    </Container>
   );
 };
 
--- src/pages/Adote/styles.js
+++ src/pages/Adote/styles.js
@@ -1,6 +1,14 @@
 import styled from "styled-components";
 
-export const ContainerTotal = styled.div`
+export const Container = styled.div`
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+export const ContentTotal = styled.div`
   width: 100%;
   height: 80vh;
   display: flex;
