634554d95b6870c7ec5b5fb3b2b9c441bb37a498
Weriks Santos <you@example.comwerikscs@gmail.com>
2022-05-18 08:31:38 -0300

    feat: add function for Adote um Pet button on home

--- src/components/section-Home-Part1/index.jsx
+++ src/components/section-Home-Part1/index.jsx
@@ -2,14 +2,20 @@ import Button from "../Button";
 import dogImg from "../../assets/dogHome.png";
 import { Section } from "./styles";
 
+import { useHistory } from "react-router-dom";
+
 const SectionHomePart1 = () => {
+  const history = useHistory();
+
   return (
     <Section>
       <div>
         <h1>
           Somos amantes de animais de estimação tentando fazer a diferença.
         </h1>
-        <Button orangeSchema>Adote um pet</Button>
+        <Button orangeSchema onClick={() => history.push("/adote")}>
+          Adote um pet
+        </Button>
       </div>
       <figcaption>
         <img src={dogImg} alt="" />
