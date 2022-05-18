e8e6e2cd9f2953cd0f9c5d08cfbc73e59ce18281
emillyalmeida <emillyalmeidan@gmail.com>
2022-05-18 17:41:42 -0300

    feat: heigth auto because was cut the background

--- src/pages/Adote/styles.js
+++ src/pages/Adote/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 100%;
-  height: 100vh;
+  height: auto;
   /* height: 80vh; */
 
   /* padding-top: 100px; */
